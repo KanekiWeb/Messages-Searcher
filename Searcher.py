2d98545d89b78611b9efd2a7a59d9592f20664d5
Kaneki <72992708+KanekiWeb@users.noreply.github.com>
2021-11-06 21:55:37 +0100

    🔍 Searcher
    🔍 Searcher

--- Searcher.py
+++ Searcher.py
@@ -44,35 +44,40 @@ async def on_ready():
     messages_count = 0
     dm_count = 0
 
-    for guild in client.guilds:
-        print(f"[{Fore.RED}SERVER{Fore.RESET}] Scan du serveur: " + guild.name+ " "*30)
-        serveur_count += 1
-        for channel in guild.text_channels:
-            print(f"[{Fore.CYAN}CHANNEL{Fore.RESET}] Scan du salon: " + channel.name + " "*30, end="\r")
+    if config['type']['servers'].lower() == "yes":
+        for guild in client.guilds:
+            print(f"[{Fore.RED}SERVER{Fore.RESET}] Scan du serveur: " + guild.name+ " "*30)
+            serveur_count += 1
+            for channel in guild.text_channels:
+                print(f"[{Fore.CYAN}CHANNEL{Fore.RESET}] Scan du salon: " + channel.name + " "*30, end="\r")
+                channels_count += 1
+                try:
+                    async for message in client.get_channel(channel.id).history(limit=99999):
+                        messages_count += 1
+                        for searchmessage in config['messages']:
+                            if searchmessage in message.content:
+                                print(f"[{Fore.GREEN}MESSAGE{Fore.RESET}] Message Found: " + message.content)
+                                open('found.txt', 'a+').write(f"{message.author.name}: {message.content}\n")
+                except:
+                    pass
+
+    if config['type']['dm'].lower() == "yes":   
+        for prv_channel in client.private_channels:
             channels_count += 1
+            dm_count += 1
+            print(f"[{Fore.CYAN}CHANNEL{Fore.RESET}] Scan du DM: {str(prv_channel).replace('Direct Message with ', '')}" + " "*30, end="\r")
+            channel = client.get_channel(prv_channel.id)
             try:
-                channel = client.get_channel(channel.id)
                 async for message in channel.history(limit=99999):
                     messages_count += 1
-                    if config['message'] in message.content:
-                        print(f"[{Fore.GREEN}MESSAGE{Fore.RESET}] Message Found: " + message.content)
-                        open('found.txt', 'a+').write(f"{message.author.name}: {message.content}\n")
+                    for searchmessage in config['messages']:
+                        if searchmessage in message.content:
+                            print(f"[{Fore.GREEN}MESSAGE{Fore.RESET}] Message Found: " + message.content)
+                            open('found.txt', 'a+').write(f"{message.author.name}: {message.content}\n")
             except:
                 pass
-    
-    for prv_channel in client.private_channels:
-        channels_count += 1
-        dm_count += 1
-        username = str(prv_channel).replace('Direct Message with ', '')
-        print(f"[{Fore.CYAN}CHANNEL{Fore.RESET}] Scan du DM: {username}" + " "*30, end="\r")
-        channel = client.get_channel(prv_channel.id)
-        async for message in channel.history(limit=99999):
-            messages_count += 1
-            if config['message'] in message.content:
-                print(f"[{Fore.GREEN}MESSAGE{Fore.RESET}] Message Found: " + message.content)
-                open('found.txt', 'a+').write(f"{message.author.name}: {message.content}\n")
 
-    input(f"{Fore.RED+str(messages_count)+Fore.RESET} Messages on été scanné depuis {Fore.RED+str(channels_count)+Fore.RESET} Salons ({Fore.RED+str(serveur_count)+Fore.RESET} Serveurs, {Fore.RED+str(dm_count)+Fore.RESET} DMs)")
+    input(f"{Fore.RED+str(messages_count)+Fore.RESET} Messages ont été scanné depuis {Fore.RED+str(channels_count)+Fore.RESET} Salons ({Fore.RED+str(serveur_count)+Fore.RESET} Serveurs, {Fore.RED+str(dm_count)+Fore.RESET} DMs)")
 
 
 client.run(config["token"], bot=False)
